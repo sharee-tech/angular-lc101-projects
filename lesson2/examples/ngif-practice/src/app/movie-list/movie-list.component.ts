188685ab137bca08be8c96c605f8e93111453e81
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2021-08-17 19:43:36 -0500

    events

--- lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+   selector: 'movie-list',
+   templateUrl: './movie-list.component.html',
+   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  movies = ['The Manchurian Candidate', 'Oceans 8', 'Hidden Figures', 'The Incredibles', 'The Princess Bride'];
+   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit() {
-  }
-
+   ngOnInit() {
+   }
 }
